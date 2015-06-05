0d527ec9d189a42f6d45f6c6df5c0c8d3064499e
wubang <wolfwubang@gmail.com>
2015-06-05 23:46:14 +0800

    Refactor the file

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,52 +1,8 @@
 package com.twu.biblioteca;
 
-import com.twu.biblioteca.service.BookService;
-
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Library");
-        init();
     }
-
-    private static void init() {
-        Menu menu = initMenu();
-        MenuItem menuItem;
-        do {
-            menuItem = menu.showMenu();
-            BookService bookService = new BookService();
-            switch (menuItem) {
-                case LISTBOOKS:
-                    bookService.listBooks();
-                    break;
-                case BORROWBOOK:
-                    bookService.borrowBook();
-                    break;
-                case LISTBORROWBOOKS:
-                    bookService.listBorrowBooks();
-                    break;
-                case RETURNBOOK:
-                    bookService.returnBook();
-                    break;
-                case QUIT:
-                    System.out.println("This System will be exit!");
-                    break;
-                case ERROR:
-                    System.out.println("You choose is not valid, please input again");
-                    break;
-            }
-        } while (menuItem != MenuItem.QUIT);
-    }
-
-    private static Menu initMenu() {
-        Menu menu = new Menu();
-        menu.addMenuItem(MenuItem.LISTBOOKS);
-        menu.addMenuItem(MenuItem.BORROWBOOK);
-        menu.addMenuItem(MenuItem.LISTBORROWBOOKS);
-        menu.addMenuItem(MenuItem.RETURNBOOK);
-        menu.addMenuItem(MenuItem.QUIT);
-        return menu;
-    }
-
-
 }
--- src/com/twu/biblioteca/model/Role.java
+++
@@ -1,8 +0,0 @@
-package com.twu.biblioteca.model;
-
-/**
- * Created by Thoughtworks on 5/31/15.
- */
-public enum Role {
-    STUDENT,AUTHOR
-}
